e29c8b110d8968a21dc6d6e71d90a779e29bb024
silvano.amaral9@gmail.com <silvano.amaral9@gmail.com>
2019-10-11 14:49:50 -0300

    alterando caminhao dist server

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.get('/api/mensagem', (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/dist'))
+  app.use(express.static('dist'))
 
   app.get('*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, '/', 'dist','index.html'))
+    res.sendFile(path.resolve(__dirname, 'dist','index.html'))
   })
 }
 
